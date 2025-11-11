b1819cabe439424dd36cf1ecb23d38b32122b850
TonmoyZohani <tonmoyzohani@gmail.com>
2025-11-11 14:21:05 +0600

    types creates

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,15 @@
-export interface ActionListBookmark {
-  id: string;
-  actionlistid: string;  
-  boardId: string;
-  userId: string;
-  createdAt: string;
-  updatedAt: string;
-}
+// export interface ActionListBookmark {
+//   id: string;
+//   actionlistid: string;  
+//   boardId: string;
+//   userId: string;
+//   createdAt: string;
+//   updatedAt: string;
+// }
 
-export interface ActionList {
-  id: string;
-  action: string;
-  actionboardID: string;
-  actionListStatus: string;
-}
+// export interface ActionList {
+//   id: string;
+//   action: string;
+//   actionboardID: string;
+//   actionListStatus: string;
+// }
