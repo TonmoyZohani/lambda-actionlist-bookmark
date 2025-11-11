b4acec0aedcb80caa2c4cc5de7d941415dfc7d41
TonmoyZohani <tonmoyzohani@gmail.com>
2025-11-11 14:47:44 +0600

    update

--- src/handlers/getBookmarksWithActionLists.ts
+++ src/handlers/getBookmarksWithActionLists.ts
@@ -7,16 +7,16 @@ const actionListService = new ActionListService();
 export const handler = async (event: { queryStringParameters: any }) => {
   const { userId, boardId } = event.queryStringParameters;
 
-  // Step 1: Get bookmarks
+  // Getting bookmarks
   const bookmarks = await bookmarkService.getBookmarksByUserAndBoard(
     userId,
     boardId
   );
 
-  // Step 2: Extract actionListIds
-  const actionListIds = bookmarks.map((bookmark) => bookmark.actionlistid); 
+  // Storing actionListIds
+  const actionListIds = bookmarks.map((bookmark) => bookmark.actionlistid);
 
-  // Step 3: Get action lists
+  // Getting action lists
   const actionLists = await actionListService.getActionListsByIds(
     actionListIds
   );
